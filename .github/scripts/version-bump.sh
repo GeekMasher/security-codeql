70cc22fa645e16486a49fe3f045c2f058cc81a58
Mathew Payne <geekmasher@gmail.com>
2021-04-22 13:49:00 +0100

    Use commits over version numbers

--- .github/scripts/version-bump.sh
+++ .github/scripts/version-bump.sh
@@ -3,15 +3,14 @@
 
 # Get current local version
 get_local_version() {
-    git submodule status codeql |
-    sed -E 's/.*\(([^"]+)\).*/\1/'
+  git submodule status codeql |
+    awk -F" " '{print $1}'
 }
 
 # Get current remote version
 get_latest_release() {
   curl --silent "https://api.github.com/repos/github/codeql/tags?per_page=1" |
-    grep '"name":' |
-    sed -E 's/.*"([^"]+)".*/\1/'
+    jq -r '.[].commit.sha'
 }
 
 # Check verions
@@ -23,21 +22,20 @@ echo "[+] Latest Release  :: $CODEQL_LATEST"
 
 # Validate versions
 if [[ "$CODEQL_LOCAL" = "$CODEQL_LATEST" ]]; then
+  echo "[+] No updates needed, matching versions"
 
-    echo "[+] No updates needed"
-    echo "[+] Matching versions :: $CODEQL_LOCAL" 
 else
-    # Update CodeQL verions
-    echo "[+] Updating to latest release..."
+  # Update CodeQL verions
+  echo "[+] Updating to latest release..."
 
-    # Update submodule branch
-    git submodule set-branch -b $CODEQL_LATEST codeql
-    git submodule set-branch -b $CODEQL_LATEST codeql-go
+  # Update submodule branch
+  git submodule set-branch -b $CODEQL_LATEST codeql
+  git submodule set-branch -b $CODEQL_LATEST codeql-go
 
-    echo "[+] Updating remote submodules"
-    git submodule update --recursive --remote
+  echo "[+] Updating remote submodules"
+  git submodule update --recursive --remote
 
-    echo "[+] New submodule status"
-    git submodule status
+  echo "[+] New submodule status"
+  git submodule status
 
 fi
