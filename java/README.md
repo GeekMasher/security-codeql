6ba733f71fc07ebd63977dd2ccd0582f03b28991
Mathew Payne <geekmasher@gmail.com>
2021-10-15 17:41:26 +0100

    Add README's, suites summaries, and script

---
+++ java/README.md
@@ -0,0 +1,14 @@
+# Java
+
+## Query Suites
+<!-- AUTOMATION -->
+| Name | Queries Count | Description | Path |
+| :--- | :---- | :--- | :--- |
+| `default` | 42 | Default Query Suite | `code-scanning` |
+| `extended` | 61 | Security Extended Suite | `security-extended` |
+| `local-variants` | 72 | Security Extended with local variants enabled | `GeekMasher/security-queries/java/suites/codeql-java-local.qls@main` |
+| `quality` | 181 | Security and Quality Extended Suite | `security-and-quality` |
+| `super-extended` | 96 | Security Extended with Experimental and Custom Queries Suite | `GeekMasher/security-queries/java/suites/codeql-java.qls@main` |
+
+
+<!-- AUTOMATION -->
---
+++ scripts/generate-coverage.py
@@ -0,0 +1,172 @@
+from ast import parse
+import os
+import json
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser("GenerateCoverage")
+parser.add_argument("-l", "--language")
+
+parser.add_argument("-o", "--output")
+parser.add_argument("--disable-cache", action="store_true")
+parser.add_argument("--output-suite")
+
+arguments = parser.parse_args()
+
+here = os.getcwd()
+codeql_folder = "codeql"
+
+default_suites = {
+    "default": {
+        "name": "Default Query Suite",
+        "path": "{language}-code-scanning.qls",
+        "type": "builtin",
+    },
+    "extended": {
+        "name": "Security Extended Suite",
+        "path": "{language}-security-extended.qls",
+        "type": "builtin",
+    },
+    "quality": {
+        "name": "Security and Quality Extended Suite",
+        "path": "{language}-security-and-quality.qls",
+        "type": "builtin",
+    },
+    "local-variants": {
+        "name": "Security Extended with local variants enabled",
+        "path": "{language}/suites/codeql-{language}-local.qls",
+    },
+    "super-extended": {
+        "name": "Security Extended with Experimental and Custom Queries Suite",
+        "path": "{language}/suites/codeql-{language}.qls",
+    },
+}
+
+
+def getFormattedString(input: str, **data: dict):
+    return input.format(**data)
+
+
+def createTable(suites: dict, language: str):
+    RETVAL = ""
+    RETVAL += "| Name | Queries Count | Description | Path |\n"
+    RETVAL += "| :--- | :---- | :--- | :--- |\n"
+
+    for suite, queries in suites.items():
+        data = default_suites.get(suite)
+        name = data.get("name")
+
+        path = getFormattedString(data.get("path"), language=language)
+
+        if data.get("type") == "builtin":
+            path = path.replace(".qls", "")
+            path = path.replace(language + "-", "", 1)
+        else:
+            path = f"GeekMasher/security-queries/{path}@main"
+
+        RETVAL += f"| `{suite}` | {len(queries)} | {name} | `{path}` |\n"
+
+    return RETVAL
+
+
+def createMarkdown(markdown_path: str, content: str):
+    PLACEHOLDER = "<!-- AUTOMATION -->"
+    with open(markdown_path, "r") as handle:
+        file_data = handle.read()
+
+    start_location = file_data.index(PLACEHOLDER) + len(PLACEHOLDER)
+    end_location = file_data.index(PLACEHOLDER, start_location)
+
+    print("Inserting data between: {} <-> {}".format(start_location, end_location))
+
+    new_data = (
+        file_data[:start_location] + "\n" + content + "\n\n" + file_data[end_location:]
+    )
+
+    with open(markdown_path, "w") as handle:
+        handle.write(new_data)
+
+
+def buildQueries(language: str):
+    suites = {}
+    for suite, suite_data in default_suites.items():
+        suite_path = getFormattedString(suite_data.get("path"), language=language)
+        print("Suite :: " + suite_path)
+
+        if suite_data.get("type") is None:
+            if not os.path.exists(suite_path):
+                print(f"Suite Path does not exist :: {suite_path}")
+                print(f"Skipping {suite}...")
+                continue
+
+        command = [
+            "codeql",
+            "resolve",
+            "queries",
+            suite_path,
+            "--format=bylanguage",
+            "--additional-packs",
+            ".",
+        ]
+
+        print(f"Processing :: '{suite}'")
+
+        output_config = suite.replace(".qls", ".json")
+        with open(output_config, "w") as handle:
+            subprocess.run(command, stdout=handle)
+
+        with open(output_config, "r") as handle:
+            query_suite_config = json.load(handle).get("byLanguage", {}).get(language)
+
+        suites[suite] = []
+        # DATA[arguments.language][suite] = query_suite_config
+
+        for query_path, _ in query_suite_config.items():
+            query_path = query_path.replace(here + "/", "")
+
+            # if not query_path.startswith(codeql_folder):
+            #     #  Non-standard CodeQL Query
+            #     print(f"Query :: " + query_path)
+
+            suites[suite].append(query_path)
+
+        print(f"Queries Count :: {len(suites[suite])}")
+        os.remove(output_config)
+
+    return suites
+
+
+if __name__ == "__main__":
+
+    DATA = {}
+
+    languages = []
+    if not arguments.language:
+        languages = ["cpp", "csharp", "java", "javascript", "python"]
+    else:
+        languages = [arguments.language]
+
+    for language in languages:
+        output_queries = os.path.join(language, "queries.json")
+        DATA[language] = {}
+
+        if not os.path.exists(output_queries) or arguments.disable_cache:
+            DATA[language] = buildQueries(language)
+        else:
+            print(f"Reading queries from cache :: {output_queries}")
+            with open(output_queries, "r") as handle:
+                DATA[language] = json.load(handle)
+
+        output = os.path.join(language, "README.md")
+
+        with open(output_queries, "w") as handle:
+            json.dump(DATA[language], handle, indent=2, sort_keys=True)
+
+        print(f"Output :: {output}")
+
+        # print(json.dumps(DATA[language]))
+
+        OUTPUT = createTable(DATA.get(language), language)
+        createMarkdown(output, OUTPUT)
+        # with open(output, "w") as handle:
+        #     handle.write(OUTPUT)
